b53d93f84c99490fd1a48eafc0a772762aedbe44
Dndyytr <dandy.taufiqurrochman@gmail.com>
2026-04-07 19:26:17 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ cd /srv/project-chiciko
 docker compose up -d --build
 
 docker compose exec -T app composer install --no-dev --optimize-autoloader
-docker compose exec -T app npm install
-docker compose exec -T app npm run build
 docker compose exec -T app php artisan migrate --force
 docker compose exec -T app php artisan config:cache
 docker compose exec -T app php artisan route:cache
