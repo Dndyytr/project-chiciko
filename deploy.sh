0444854dd9c77057c9fdd7a0b1e90050d9d326b1
Dndyytr <dandy.taufiqurrochman@gmail.com>
2026-04-07 21:10:45 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ cd /srv/project-chiciko
 
 docker compose up -d --build
 
+# Tunggu MySQL siap
+until docker compose exec mysql mysqladmin ping -h "localhost" --silent; do
+  echo "Waiting for MySQL..."
+  sleep 2
+done
+
 docker compose exec -T app composer install --no-dev --optimize-autoloader
 docker compose exec -T app php artisan migrate --force
 docker compose exec -T app php artisan config:cache
