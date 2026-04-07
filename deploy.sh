5a305dc5e283aaf52a4bba9e5d5168f084f14877
Dndyytr <dandy.taufiqurrochman@gmail.com>
2026-04-07 21:18:22 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ until docker compose exec mysql mysqladmin ping -h "localhost" --silent; do
 done
 
 docker compose exec -T app composer install --no-dev --optimize-autoloader
+docker compose exec -T app php artisan config:clear
 docker compose exec -T app php artisan migrate --force
-docker compose exec -T app php artisan config:cache
-docker compose exec -T app php artisan route:cache
-docker compose exec -T app php artisan view:cache
+docker compose exec -T app php artisan optimize:clear
